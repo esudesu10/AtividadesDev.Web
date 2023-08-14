0e6696515f822cd8e67e0e58b7aa033c8aa0aa13
Eduardo R Vieira <eduardovieirayt@gmail.com>
2023-08-14 13:05:43 -0300

    Atividade de NumeroPrimo

---
+++ NumeroPrimo.js
@@ -0,0 +1,23 @@
+function isPrime(number) {
+  if (number <= 1) {
+    return false;  // Números menores ou iguais a 1 não são primos
+  }
+  for (let i = 2; i * i <= number; i++) {
+    if (number % i === 0) {
+      return false;  // Se for divisível por algum número entre 2 e a raiz quadrada, não é primo
+    }
+  }
+  return true;  // Se não foi encontrado divisor entre 2 e a raiz quadrada, é primo
+}
+
+function printIsPrime(number) {
+  if (isPrime(number)) {
+    console.log("1 - O número é primo.");
+  } else {
+    console.log("0 - O número não é primo.");
+  }
+}
+
+// Exemplo de uso
+const inputNumber = 0;  // Altere este valor para o número que você deseja verificar
+printIsPrime(inputNumber);
